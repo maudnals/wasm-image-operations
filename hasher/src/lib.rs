d1851b090e575c8d779180333529f8ae3dd504d5
maudnals <maud.nalpas@gmail.com>
2018-11-09 02:27:01 +0100

    feat: scaffolding

---
+++ hasher/src/lib.rs
@@ -0,0 +1,15 @@
+#![feature(use_extern_macros)]
+
+#[macro_use]
+extern crate stdweb;
+extern crate sha1;
+
+use stdweb::js_export;
+use sha1::Sha1;
+
+#[js_export]
+fn sha1( string: &str ) -> String {
+    let mut hasher = Sha1::new();
+    hasher.update( string.as_bytes() );
+    hasher.digest().to_string()
+}
