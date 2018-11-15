ad1d7e450ff19244b73eafe76e2aa72ee797dc84
maudnals <maud.nalpas@gmail.com>
2018-11-15 22:43:57 +0100

    feat(rs lib): added support for typed arrays inputs

--- hasher/src/lib.rs
+++ hasher/src/lib.rs
@@ -4,7 +4,13 @@
 extern crate stdweb;
 extern crate sha1;
 
-use stdweb::js_export;
+use stdweb::{
+    Array,
+    js_export
+};
+use stdweb::web::{
+    TypedArray
+};
 use sha1::Sha1;
 
 #[js_export]
@@ -13,3 +19,49 @@ fn sha1( string: &str ) -> String {
     hasher.update( string.as_bytes() );
     hasher.digest().to_string()
 }
+
+#[js_export]
+fn reduce(arr: Array) -> f64 {
+    // return arr.fold(0, |sum, x| sum + x);
+    return 123.0;
+}
+
+#[js_export]
+fn reduce_c(arr: TypedArray::<u8>) -> TypedArray::<u8> {
+    // stdweb::initialize();
+    // let b = TypedArray::<f64>::from(&arr);
+    // let vertices = TypedArray::<u8>::from(&[
+    //     255, 132][..]);
+    // let k = TypedArray::<u8>::from(&arr);
+    // return reduce8([1, 2, 3])
+    // stdweb::console!(log, "IIIIII");
+    // return arr.fold(0, |sum, x| sum + x);
+    return arr;
+}
+
+// #[js_export]
+// fn reduce8(arr: TypedArray<f64>) -> f64 {
+//     return 456.0;
+// }
+
+// #[js_export]
+// fn reduceU8(arr: stdweb::web::TypedArray) -> f64 {
+//     return 123.0;
+// }
+
+// use stdweb::web::UnsafeTypedArray;
+// use 
+// use math::round;
+
+// #[no_mangle]
+// pub fn add(a: f64, b: f64) -> f64 {
+//   return a + b
+// }
+
+// #[no_mangle]
+// pub fn avg(arr: UnsafeTypedArray<f64>) -> UnsafeTypedArray<f64> {
+//   // let a = vec![4, 5, 6];
+//   // println!("hello there!");
+//   // let sum = arr.iter().fold(0, |acc, x| acc + x);
+//   return arr;
+// }
--- main.js
+++ main.js
@@ -4,6 +4,29 @@ var input = document.getElementById("input");
 var output = document.getElementById("output");
 output.innerText = hasher.sha1(input.value);
 
+const img = document.querySelector("#img");
+
 input.addEventListener("keyup", function(event) {
   output.innerText = hasher.sha1(input.value);
 });
+
+const canvas = document.createElement("canvas");
+canvas.width = img.width;
+canvas.height = img.height;
+canvas.getContext("2d").drawImage(img, 0, 0, img.width, img.height);
+const arrData = canvas
+  .getContext("2d")
+  .getImageData(0, 0, img.width, img.height).data;
+
+console.log(arrData);
+console.log(Array.from(arrData.slice(0, 8)));
+console.log(arrData.slice(0, 8));
+
+console.log(hasher.reduce([1, 2, 3]));
+// console.log(hasher.reduce_c([1.0]));
+// console.log(hasher.reduce_c(arrData));
+
+// can not use clamped arrays!!!
+console.log(hasher.reduce_c(new Uint8Array(arrData)));
+
+// console.log(hasher.reduce(Array.from(arrData)));
